7028586aac47653f973fb2478d1b4963b0066b1f
kn1515 <nakata1789345@gmail.com>
2025-12-28 16:25:03 +0900

    About セクションの実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Layout from "./components/Layout";
 import type { NavItem } from "./types/navigation";
 import Profile from "./components/Profile";
+import About from "./components/About";
 
 const App = () => {
   const [activeTab, setActiveTab] = useState<NavItem>("Profile");
@@ -13,7 +14,7 @@ const App = () => {
   return (
     <Layout handleTabClick={handleTabClick} title={activeTab}>
       {activeTab === "Profile" && <Profile />}
-      {activeTab === "About" && <p>About</p>}
+      {activeTab === "About" && <About />}
       {activeTab === "Skill" && <p>Skill</p>}
       {activeTab === "Contact" && <p>Contact</p>}
     </Layout>
