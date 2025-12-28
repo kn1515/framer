ba57d1d7c5aa483cf9e15e14f912625b5f65a1af
kn1515 <nakata1789345@gmail.com>
2025-12-28 16:17:40 +0900

    ベースレイアウトの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Layout from "./components/Layout";
+import type { NavItem } from "./types/navigation";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [activeTab, setActiveTab] = useState<NavItem>("Profile");
+
+  const handleTabClick = (tab: NavItem) => {
+    setActiveTab(tab);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Layout handleTabClick={handleTabClick} title={activeTab}>
+      {activeTab === "Profile" && <p>Profile</p>}
+      {activeTab === "About" && <p>About</p>}
+      {activeTab === "Skill" && <p>Skill</p>}
+      {activeTab === "Contact" && <p>Contact</p>}
+    </Layout>
+  );
+};
 
-export default App
+export default App;
